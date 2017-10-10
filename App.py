07d711866d10bf02a65fae997e6a8f367870d3c0
Dmitrii_Valiaev <dmitrii_valiaev@epam.com>
2017-10-10 16:22:54 +0300

    1) Create db 2) Import libraries 3) Create search requests 4) Create files for deploying

---
+++ App.py
@@ -0,0 +1,96 @@
+import os
+import googlemaps
+import DBqueries
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+gmaps = googlemaps.Client(key='AIzaSyDXNQpAsqsaii_Xxvu5tRf6dfHSasLl1WI')
+conn = None
+
+
+def start(bot, update):
+    update.message.reply_text('Hi! This is Uneatable bot!')
+    update.message.reply_text('Send /help for additional information!')
+    user = DBqueries.select_user(update.message.from_user.id)
+    if user is None:
+        update.message.reply_text('Oh! You are a new user! Thank you for joining us!')
+        update.message.reply_text('Please, send your location!')
+        DBqueries.insert_user(update.message.from_user.id, None, None)
+    else:
+        update.message.reply_text('Send the name of the place or name of the dish!')
+
+
+def update_location(bot, update):
+    update.message.reply_text('We are going to reset your location!')
+    DBqueries.update_user(update.message.from_user.id, None, None)
+    update.message.reply_text('Please, send your new location!')
+
+
+def help(bot, update):
+    update.message.reply_text('Send /location to change your current location!')
+
+
+def location(bot, update):
+    user_location = update.message.location
+    lat = user_location['latitude']
+    long = user_location['longitude']
+    DBqueries.update_user(update.message.from_user.id, lat, long)
+    update.message.reply_text('Now you can send the name of the place or name of the dish!')
+
+
+def places(bot, update):
+    user_id = update.message.from_user.id
+    types = ['food', 'bar', 'cafe']
+
+    user = DBqueries.select_user(user_id)
+    if user[1] is None or user[2] is None:
+        update.message.reply_text('Please, send your location!')
+        return
+    else:
+        lat = user[1]
+        long = user[2]
+
+    if len(update.message.text) == 1:
+        emoji = DBqueries.select_emoji(update.message.text)
+        if emoji is not None:
+            update.message.text = emoji[0]
+        else:
+            update.message.reply_text('Wrong message! It is empty or you send wrong emoji! Try again!')
+            return
+
+    directions_result = gmaps.places_nearby(location=(lat, long), radius='5000', keyword=update.message.text,
+                                            type=types)
+
+    if len(directions_result['results']) == 0:
+        update.message.reply_text('No restaurants were found! Try again!')
+        return
+
+    global List
+    List = "List of restaurants:\n"
+    for direct in directions_result['results']:
+        List += "🍽 " + direct['name'] + " | " + direct['vicinity'] + "\n"
+    update.message.reply_text(List)
+    update.message.reply_text('Have a nice meal! 🍴')
+
+
+def main():
+    token = "385433489:AAHef764SCjINLmZXNSqemyMIAWWW_mCK_8"
+    port = int(os.environ.get('PORT', '5000'))
+    updater = Updater(token)
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("location", update_location))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(MessageHandler(Filters.text, places))
+    dp.add_handler(MessageHandler(Filters.location, location))
+    updater.start_webhook(listen="0.0.0.0",
+                          port=port,
+                          url_path=token)
+
+    updater.bot.set_webhook("https://uneatable.herokuapp.com/" + token)
+    updater.idle()
+
+
+if __name__ == '__main__':
+    conn = DBqueries.connect_to_db()
+    main()
---
+++ DBqueries.py
@@ -0,0 +1,48 @@
+import psycopg2
+
+conn = None
+
+
+def connect_to_db():
+    try:
+        global conn
+        conn = psycopg2.connect("dbname='d2h2b8ktkdsumv'"
+                                " user='wewtulwwvdturz' "
+                                "host='ec2-23-21-189-181.compute-1.amazonaws.com' "
+                                "password='09c1c710a5873351239fcb5002e53085ea683cfc29f40f23ed5ba9bc0e03770f'")
+    except:
+        print("I am unable to connect to the database")
+
+
+def select_user(userd_id):
+    cur = conn.cursor()
+    cur.execute("select * from users where user_id = " + str(userd_id) + ";")
+    user = cur.fetchone()
+    cur.close()
+    return user
+
+
+def insert_user(user_id, lat, long):
+    cur = conn.cursor()
+    cur.execute("insert into users values(%s,%s,%s)", (user_id, lat, long))
+    conn.commit()
+    cur.close()
+
+
+def update_user(user_id, lat, long):
+    cur = conn.cursor()
+    cur.execute("update users set lat = %s, long = %s where user_id = %s", (lat, long, user_id))
+    conn.commit()
+    cur.close()
+
+
+def select_emoji(emoji):
+    cur = conn.cursor()
+    cur.execute("select description from emojis where emoji = %s", (emoji))
+    emoji = cur.fetchone()
+    cur.close()
+    return emoji
+
+
+def close_connection():
+    conn.close()
